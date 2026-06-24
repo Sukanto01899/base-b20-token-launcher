4183d0c171e55579561e21c2b1f452a26cb60437
sukanto01899 <sukanto01899@gmail.com>
2026-06-24 14:41:40 +0600

    vercel Analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Providers } from "./providers";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -32,6 +33,7 @@ export default function RootLayout({
           {children}
           <Footer />
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
